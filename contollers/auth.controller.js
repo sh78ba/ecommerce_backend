faf68153fc7753f6d74213a67723f0a39291c40c
sh78ba <sh123tu@gmail.com>
2024-06-04 19:09:13 +0530

    signin using jwt token

--- contollers/auth.controller.js
+++ contollers/auth.controller.js
@@ -4,6 +4,8 @@ Controller to register a user
 
 const bcrypt=require("bcryptjs")
 const user_model=require("../models/user.model")
+const secret=require("../configs/auth.config")
+const jwt=require("jsonwebtoken")
 
 exports.signup=async(req,res)=>{
     //read the request bidy
@@ -41,5 +43,37 @@ exports.signup=async(req,res)=>{
     }
 
     //return the response back to user
+}
+
+
+exports.signin=async(req,res)=>{
+//check if userid is present
+const user=await user_model.findOne({userId:req.body.userId})
+if(user==null){
+return res.status(400).send({
+    message:"User Id passed is not a valid user id"
+})
+}
+
+//check if password is correct
+const isPasswordValid=bcrypt.compareSync(req.body.password,user.password)
+if(!isPasswordValid){
+    res.status(401).send({
+        message:"Wrong password passed"
+    })
+}
+
+//using jwt create access token with given TTL and return
+const token=jwt.sign({id:user.userId},secret.secret,{
+    expiresIn:120
+})
+
+res.status(200).send({
+    name : user.name,
+    userId : user.userId,
+    email:user.email,
+    userType:user.userType,
+    accessToken: token
+})
 
 }
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -8,3 +8,7 @@ const authMw=require("../middlewares/auth.mw")
 module.exports=(app)=>{
     app.post("/ecomm/api/v1/auth/signup",[authMw.verifySignUpBody],authController.signup)
 }
+
+module.exports=(app)=>{
+    app.post("/ecomm/api/v1/auth/signin",authController.signin)
+}
